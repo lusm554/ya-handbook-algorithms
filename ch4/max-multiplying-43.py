9876c3ab3c0a5cec27bd470b9ad2597fb5b4ea62
lusm <loveyousomuch554@gmail.com>
2025-01-02 15:19:05 +0300

    max multiplying 4.3

--- ch4/max-multiplying-43.py
+++ ch4/max-multiplying-43.py
@@ -2,5 +2,15 @@ n = int(input())
 a = [int(x) for x in input().split()]
 
 # simple solution
-res = a.pop(a.index(max(a))) * max(a)
-print(res)
+#res = a.pop(a.index(max(a))) * max(a)
+#print(res)
+
+# using heap
+import heapq
+heap = list()
+for i in a:
+  heapq.heappush(heap, i)
+  if len(heap) > 2: # for less memory use
+    heapq.heappop(heap)
+m1, m2 = heapq.nlargest(2, heap)
+print(m1*m2)
