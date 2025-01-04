a301825bd1cf5c46d50d13e001a449b23b9bff9b
lusm <loveyousomuch554@gmail.com>
2025-01-04 21:22:39 +0300

    max mul of 3 digits 4.3

--- ch4/max-3-multi-43.py
+++ ch4/max-3-multi-43.py
@@ -1,16 +1,10 @@
+"""
+Дана последовательность целых чисел.
+"""
 n = int(input())
 a = [int(x) for x in input().split()]
 
-# simple solution
-#res = a.pop(a.index(max(a))) * max(a)
-#print(res)
-
-# using heap
-import heapq
-heap = list()
-for i in a:
-  heapq.heappush(heap, i)
-  if len(heap) > 3: # for less memory use
-    heapq.heappop(heap)
-m1, m2, m3 = heapq.nlargest(3, heap)
-print(m1*m2*m3)
+def max_triple_mul(lst):
+    lst = sorted(lst)
+    return max(lst[-1]*lst[-2]*lst[-3], lst[0]*lst[1]*lst[-1])
+print(max_triple_mul(a))
